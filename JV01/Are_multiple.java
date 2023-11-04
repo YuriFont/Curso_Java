8957e7b024e895a5f305f732689289df130b9699
YuriFont <yurifontenelesilva180303@gmail.com>
2023-11-04 14:11:02 -0300

    Update

---
+++ JV01/Are_multiple.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class Are_multiple {
+    public static void main(String args[])
+    {
+        Scanner sc = new Scanner(System.in);
+
+        int a, b;
+
+        a = sc.nextInt();
+        b = sc.nextInt();
+
+        if (a % b == 0 || b % a == 0)
+            System.err.println("São multiplos");
+        else
+            System.out.println("Não são multiplos");
+
+        sc.close();
+    }
+}
