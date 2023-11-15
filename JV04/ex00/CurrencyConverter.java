4149031344c54db9b727ebdab6880a24855e849c
Yuri Fontenele <yurifontenelesilva180303@gmail.com>
2023-11-15 13:20:02 -0300

    Update

--- JV04/ex00/CurrencyConverter.java
+++ JV04/ex00/CurrencyConverter.java
@@ -1,8 +1,11 @@
 public class CurrencyConverter
 {
+
+    public static final double IOF = 0.06;
+
     public static double ConverterDolar(double dollar, double value)
     {
         double valueWTax = value * dollar;
-        return (valueWTax + (valueWTax * 6 / 100));
+        return (valueWTax + (valueWTax * IOF));
     }
 }
