978d4a4fe1afd2d7399b04985dccc90a79350df7
Yuri Fontenele <yurifontenelesilva180303@gmail.com>
2024-01-01 18:50:25 -0300

    Update

--- JV14/model/entities/Reservation.java
+++ JV14/model/entities/Reservation.java
@@ -1,9 +1,12 @@
 package model.entities;
 
+import java.text.SimpleDateFormat;
 import java.util.Date;
+import java.util.concurrent.TimeUnit;
 
 public class Reservation
 {
+    private static SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
     private Integer roomNumber;
     private Date checkIn;
     private Date checkOut;
@@ -34,5 +37,31 @@ public class Reservation
         return checkOut;
     }
 
-    
+    public long duration()
+    {
+        long diff = checkOut.getTime() - checkIn.getTime();
+        return (TimeUnit.DAYS.convert(diff, TimeUnit.MILLISECONDS));
+    }
+
+    public void updateDates(Date checkIn, Date checkOut)
+    {
+        this.checkIn = checkIn;
+        this.checkOut = checkOut;
+    }
+
+    @Override
+    public String toString()
+    {
+        StringBuilder sb = new StringBuilder();
+        sb.append("Rservation: Room ");
+        sb.append(getRoomNumber());
+        sb.append(", check-in: ");
+        sb.append(sdf.format(checkIn));
+        sb.append(", check-out: ");
+        sb.append(sdf.format(checkOut));
+        sb.append(", ");
+        sb.append(duration());
+        sb.append(" nights");
+        return (sb.toString());
+    }
 }
