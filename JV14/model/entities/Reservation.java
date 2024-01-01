64846466f524d144e2ca38b339183f35c0b22c4c
Yuri Fontenele <yurifontenelesilva180303@gmail.com>
2024-01-01 18:35:20 -0300

    Update

--- JV14/model/entities/Reservation.java
+++ JV14/model/entities/Reservation.java
@@ -1,6 +1,36 @@
 package model.entities;
 
+import java.util.Date;
+
 public class Reservation
 {
+    private Integer roomNumber;
+    private Date checkIn;
+    private Date checkOut;
+
+    public Integer getRoomNumber() {
+        return roomNumber;
+    }
+
+    public void setRoomNumber(Integer roomNumber) {
+        this.roomNumber = roomNumber;
+    }
+
+    public Date getCheckIn() {
+        return checkIn;
+    }
+
+    public void setCheckIn(Date checkIn) {
+        this.checkIn = checkIn;
+    }
+
+    public Date getCheckOut() {
+        return checkOut;
+    }
+
+    public void setCheckOut(Date checkOut) {
+        this.checkOut = checkOut;
+    }
+
     
 }
