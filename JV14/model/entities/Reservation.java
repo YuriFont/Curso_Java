6d6aa5c54e4858541fee05a86c2abc41ea30d724
Yuri Fontenele <yurifontenelesilva180303@gmail.com>
2024-02-05 11:24:48 -0300

    Update

--- JV14/model/entities/Reservation.java
+++ JV14/model/entities/Reservation.java
@@ -4,6 +4,8 @@ import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.concurrent.TimeUnit;
 
+import model.exceptions.DomainException;
+
 public class Reservation
 {
     private static SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy");
@@ -43,8 +45,17 @@ public class Reservation
         return (TimeUnit.DAYS.convert(diff, TimeUnit.MILLISECONDS));
     }
 
-    public void updateDates(Date checkIn, Date checkOut)
+    public void updateDates(Date checkIn, Date checkOut) throws DomainException
     {
+        Date now = new Date();
+        if (checkIn.before(now) || checkOut.before(now))
+        {
+            throw new DomainException("Reservation dates for update must be future dates");
+        }
+        if (!checkOut.after(now))
+        {
+            throw new DomainException("Check-out date must be after check-in date");
+        }
         this.checkIn = checkIn;
         this.checkOut = checkOut;
     }
