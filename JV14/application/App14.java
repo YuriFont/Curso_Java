fc8e9eb8353b3ac4301849dd87c523db175b1092
Yuri Fontenele <yurifontenelesilva180303@gmail.com>
2024-02-05 17:38:08 -0300

    Update

--- JV14/application/App14.java
+++ JV14/application/App14.java
@@ -1,9 +1,28 @@
 package application;
 
+import java.text.ParseException;
+
+import model.exceptions.DomainException;
+
 public class App14
 {
     public static void main(String[] args)
     {
-        
+        try
+        {
+            
+        }
+        catch (ParseException e)
+        {
+
+        }
+        catch (DomainException e)
+        {
+
+        }
+        catch (RuntimeException e)
+        {
+
+        }
     }
 }
